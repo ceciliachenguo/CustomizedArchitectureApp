b30e988121493592748f7dd44211557cf690831f
CeciliaChen <cecilia.g.chen@outlook.com>
2025-01-11 18:51:20 +0800

    Initial Commit

---
+++ CustomizedArchitecture/CustomizedArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomizedArchitectureApp.swift
+//  CustomizedArchitecture
+//
+//  Created by Cecilia Chen on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomizedArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
